a7f8d54ed22c75bdf598027a6335bfc5857a0ba3
Greg Weispfenning <weisy06@gmail.com>
2023-11-13 21:30:57 -0600

    first entry

---
+++ Guessnumber.py
@@ -0,0 +1,13 @@
+print("Please think of a number between 0 and 100!")
+
+guesscheck = False
+Max = 100
+Min = 0
+Guess = 50
+Response =  ""
+while(not guesscheck): 
+    print("Is your number", Guess, end=" ")
+    Response = input("Enter l for low, h for high, or c for correct guess")
+    if(Response = l)
+        Guess = 
+    
