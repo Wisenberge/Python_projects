18110a547db3ea964959f1e86b92d2a470986422
Greg Weispfenning <weisy06@gmail.com>
2023-11-19 16:46:21 -0600

    finished

--- Guessnumber.py
+++ Guessnumber.py
@@ -1,3 +1,4 @@
+#bisection search
 print("Please think of a number between 0 and 100!")
 
 guesscheck = False
@@ -5,11 +6,20 @@ Max = 100
 Min = 0
 Guess = 50
 Response =  ""
-while(not guesscheck): 
-    print("Is your number", Guess, end=" ")
-    Response = input("Enter l for low, h for high, or c for correct guess")
+while(not guesscheck):
+    print("Is your number", Guess, "?", end=" ")
+    Response = input("Enter l for low, h for high, or c for correct guess. ")
     if(Response == 'l'):
-        Guess = 50
-        print('test')
+        Min = Guess
+        Guess = int((Max + Min)/2)
+    elif(Response == 'h'):
+        Max = Guess
+        Guess = int((Max + Min)/2)
+    elif(Response == 'c'):
+        print("Game over. Your secret number was", Guess)
+        guesscheck = 1
+    else:
+        print("Sorry, I did not understand your input.")
+
         
     
